48ba1b7cebc201b61a79a161b894aa947b95fcf9
JuJu <career.j.taylor@gmail.com>
2017-11-13 21:33:18 -0600

    Building Class Properties

---
+++ TMCM/TmCM.BL/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMCM.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {   
+                // Any code here
+                return _lastName;
+            }
+            set
+            {
+                // Any code here
+                _lastName = value;
+            }
+        }
+
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
+    }
+}
